c1e1a4b3e4152c32537e4fedda12ffd944b73a14
Pierre Arquevaux <pierre.arquevaux@gmail.com>
2022-12-05 12:02:49 +0100

    User

--- src/main/java/fr/certif/model/User.java
+++ src/main/java/fr/certif/model/User.java
@@ -1,5 +1,39 @@
 package fr.certif.model;
 
-public class User {
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="utilisateur")
+
+public class User implements Serializable{
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private long id;
+		
+		@Column(nullable = false, length = 20, name ="username")
+		private String username;
+		
+		@ManyToOne(fetch=FetchType.LAZY)
+		private Message message;
 
 }
