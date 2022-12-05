9a60194737e4d64849da72071edaf54712cd8713
Pierre Arquevaux <pierre.arquevaux@gmail.com>
2022-12-05 10:32:20 +0100

    Spring Project Init

---
+++ src/main/java/fr/certif/SpringCertifApplication.java
@@ -0,0 +1,13 @@
+package fr.certif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCertifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCertifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/certif/SpringCertifApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.certif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCertifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
