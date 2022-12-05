810eeb74abbc63e139891c6e9bb04a9f2a084248
Pierre Arquevaux <pierre.arquevaux@gmail.com>
2022-12-05 13:38:10 +0100

    Message en cours, pb liaison User & channel

--- src/main/java/fr/certif/controller/MessageController.java
+++ src/main/java/fr/certif/controller/MessageController.java
@@ -1,17 +1,57 @@
 package fr.certif.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.certif.model.Message;
 import fr.certif.service.MessageService;
 
 @RestController
-@RequestMapping(path="/message")
+@RequestMapping(path = "/message")
 public class MessageController {
-	
+
 	@Autowired
 	MessageService messageService;
-	
 
+	/*
+	 * Exemple avec JPA
+	 */
+
+	@GetMapping(path = "/list", // path / url
+			produces = { "application/json" } // négociation de contenu / par défaut JSON
+	)
+	public List<Message> getMessages() {
+
+		return messageService.getAll();
+
+	}
+
+	@PostMapping(path = "/post", // path / url
+			consumes = { "application/json" } // négociation de contenu / par défaut JSON
+	)
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void getMessageJpa(@RequestBody Message message) {
+
+		messageService.saveMessage(message);
+
+	}
+
+	@DeleteMapping(path = "/delete/{id}")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void deletUser(@PathVariable ("id") Long id) {
+
+		messageService.deleteMessage(id);
+		System.out.println("utilisateur effacé");
+
+	}
 }
